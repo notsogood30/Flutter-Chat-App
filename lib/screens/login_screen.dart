3cf23c12f22f4351ce4a5cb53e9ba4f0c2528039
Ritika Gupta <65974261+notsogood30@users.noreply.github.com>
2021-09-29 00:07:53 +0530

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -30,7 +30,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   tag: 'logo',
                   child: Container(
                     height: 200.0,
-                    child: Image.asset('images/logo.png'),
+                    child: Image.asset('images/logo.png') ,
                   ),
                 ),
               ),
